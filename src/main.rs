0aee65d26498c5dc347637166696295da0f5719b
Taylor Yu <code@argon.blue>
2026-05-11 14:50:43 -0500

    edges

--- src/main.rs
+++ src/main.rs
@@ -190,6 +190,131 @@ const CORNER_TURNS: [Corners; 18] = [
     corners![(3, 2), (1, 0), (2, 0), (4, 1), (7, 2), (5, 0), (6, 0), (0, 1)], // L3
 ];
 
+// Lower 4 bits for id, bit 4 for flip
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+struct Edge(u8);
+impl Display for Edge {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        let (id, flip) = (self.0 as usize & 0x0f, (self.0 as usize & 0x10) >> 4);
+        let s = EDGES_SINGMASTER[id];
+        write!(f, "{}{}", &s[flip..], &s[..flip])
+    }
+}
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Edges([Edge; 12]);
+static EDGES_SINGMASTER: [&str; 12] = [
+    "UB", "UR", "UF", "UL",
+    "BL", "BR", "FR", "FL",
+    "DF", "DR", "DB", "DL",
+];
+const EDGES_IDENT: Edges = const {
+    let mut out = Edges([Edge(0); 12]);
+    let mut i = 0;
+    while i < 12 {
+        out.0[i as usize] = Edge(i);
+        i += 1;
+    }
+    out
+};
+impl Default for Edges {
+    fn default() -> Self {
+        EDGES_IDENT
+    }
+}
+impl Display for Edges {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        let mut first = true;
+        for x in self.0 {
+            if !first { write!(f, " ")?; }
+            first = false;
+            write!(f, "{x}")?;
+        }
+        Ok(())
+    }
+}
+impl Index<u8> for Edges {
+    type Output = Edge;
+    fn index(&self, i: u8) -> &Edge {
+        &self.0[i as usize]
+    }
+}
+impl IndexMut<u8> for Edges {
+    fn index_mut(&mut self, i: u8) -> &mut Edge {
+        &mut self.0[i as usize]
+    }
+}
+impl Mul for Edges {
+    type Output = Edges;
+    fn mul(self, rhs: Self) -> Edges {
+        Edges(rhs.0.map(|x| Edge(self[x.0 & 0xf].0 ^ (x.0 & 0x10))))
+    }
+}
+impl<'a> Mul<&'a Edges> for &'a Edges{
+    type Output = Edges;
+    fn mul(self, rhs: Self) -> Edges {
+        *self * *rhs
+    }
+}
+impl Not for Edges {
+    type Output = Edges;
+    fn not(self) -> Self {
+        let mut out = Edges([Edge(0); 12]);
+        for i in 0..12 {
+            let e = self[i].0;
+            out[e & 0xf] = Edge(i | (e & 0x10));
+        }
+        out
+    }
+}
+impl Not for &Edges {
+    type Output = Edges;
+    fn not(self) -> Edges { !*self }
+}
+impl Edges {
+    fn turn(&self, t: Turn) -> Edges {
+        *self * EDGE_TURNS[t]
+    }
+    fn turns(&self, t: &[Turn]) -> Edges {
+        let mut out = *self;
+        for x in t {
+            out = out.turn(*x);
+        }
+        out
+    }
+}
+
+macro_rules! edges {
+    ($(($id:expr, $flip:expr)),*) => {
+        Edges([
+            $( Edge($id | ($flip << 4)), )*
+        ])
+    }
+}
+static EDGE_TURNS: [Edges; 18] = [
+    edges![(3, 0), (0, 0), (1, 0), (2, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0), (11, 0)], // U1
+    edges![(2, 0), (3, 0), (0, 0), (1, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0), (11, 0)], // U2
+    edges![(1, 0), (2, 0), (3, 0), (0, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0), (11, 0)], // U3
+
+    edges![(0, 0), (6, 0), (2, 0), (3, 0), (4, 0), (1, 0), (9, 0), (7, 0), (8, 0), (5, 0), (10, 0), (11, 0)], // R1
+    edges![(0, 0), (9, 0), (2, 0), (3, 0), (4, 0), (6, 0), (5, 0), (7, 0), (8, 0), (1, 0), (10, 0), (11, 0)], // R2
+    edges![(0, 0), (5, 0), (2, 0), (3, 0), (4, 0), (9, 0), (1, 0), (7, 0), (8, 0), (6, 0), (10, 0), (11, 0)], // R3
+
+    edges![(0, 0), (1, 0), (7, 1), (3, 0), (4, 0), (5, 0), (2, 1), (8, 1), (6, 1), (9, 0), (10, 0), (11, 0)], // F1
+    edges![(0, 0), (1, 0), (8, 0), (3, 0), (4, 0), (5, 0), (7, 0), (6, 0), (2, 0), (9, 0), (10, 0), (11, 0)], // F2
+    edges![(0, 0), (1, 0), (6, 1), (3, 0), (4, 0), (5, 0), (8, 1), (2, 1), (7, 1), (9, 0), (10, 0), (11, 0)], // F3
+
+    edges![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (11, 0), (8, 0), (9, 0), (10, 0)], // D1
+    edges![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (10, 0), (11, 0), (8, 0), (9, 0)], // D2
+    edges![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (9, 0), (10, 0), (11, 0), (8, 0)], // D3
+
+    edges![(5, 1), (1, 0), (2, 0), (3, 0), (0, 1), (10, 1), (6, 0), (7, 0), (8, 0), (9, 0), (4, 1), (11, 0)], // B1
+    edges![(10, 0), (1, 0), (2, 0), (3, 0), (5, 0), (4, 0), (6, 0), (7, 0), (8, 0), (9, 0), (0, 0), (11, 0)], // B2
+    edges![(4, 1), (1, 0), (2, 0), (3, 0), (10, 1), (0, 1), (6, 0), (7, 0), (8, 0), (9, 0), (5, 1), (11, 0)], // B3
+
+    edges![(0, 0), (1, 0), (2, 0), (4, 0), (11, 0), (5, 0), (6, 0), (3, 0), (8, 0), (9, 0), (10, 0), (7, 0)], // L1
+    edges![(0, 0), (1, 0), (2, 0), (11, 0), (7, 0), (5, 0), (6, 0), (4, 0), (8, 0), (9, 0), (10, 0), (3, 0)], // L2
+    edges![(0, 0), (1, 0), (2, 0), (7, 0), (3, 0), (5, 0), (6, 0), (11, 0), (8, 0), (9, 0), (10, 0), (4, 0)], // L3
+];
 fn main() {
     use Turn::*;
     let corners = Corners::default();
@@ -204,11 +329,35 @@ fn main() {
     println!("R1U1R3U3R1U1R3U3: {}", corners.turns(&[R1, U1, R3, U3, R1, U1, R3, U3]));
     println!("sledge: {}", corners.turns(&[R3, F1, R1, F3]));
 
+    let edges = Edges::default();
+    println!("{edges}");
+    println!("U1: {}", edges.turn(U1));
+    println!("U2: {}", edges.turn(U2));
+    println!("U3: {}", edges.turn(U3));
+    println!("R1: {}", edges.turn(R1));
+    println!("R2: {}", edges.turn(R2));
+    println!("R3: {}", edges.turn(R3));
+
+    println!("sledge: {}", edges.turns(&[R3, F1, R1, F3]));
+
     assert_eq!(corners.turn(U2), corners.turn(U2).invert());
     assert_eq!(corners.turn(U3), corners.turn(U1).invert());
     assert_eq!(corners.turn(R2), corners.turn(R2).invert());
     assert_eq!(corners.turn(R3), corners.turn(R1).invert());
     assert_eq!(corners.turn(F3), !corners.turn(F1));
+
+    assert_eq!(edges.turn(U3), !edges.turn(U1));
+    assert_eq!(edges.turn(U2), !edges.turn(U2));
+    assert_eq!(edges.turn(R3), !edges.turn(R1));
+    assert_eq!(edges.turn(R2), !edges.turn(R2));
+    assert_eq!(edges.turn(F3), !edges.turn(F1));
+    assert_eq!(edges.turn(F2), !edges.turn(F2));
+    assert_eq!(edges.turn(D3), !edges.turn(D1));
+    assert_eq!(edges.turn(D2), !edges.turn(D2));
+    assert_eq!(edges.turn(B3), !edges.turn(B1));
+    assert_eq!(edges.turn(B2), !edges.turn(B2));
+    assert_eq!(edges.turn(L3), !edges.turn(L1));
+    assert_eq!(edges.turn(L2), !edges.turn(L2));
 
     let s = corners.turns(&[R1, U1, R3, U3]);
     assert_eq!(s * s, corners.turns(&[R1, U1, R3, U3, R1, U1, R3, U3]));
