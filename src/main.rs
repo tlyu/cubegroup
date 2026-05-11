5fd2677736d5b577d95d98d2030fddd71b25b030
Taylor Yu <code@argon.blue>
2026-05-11 18:15:16 -0500

    move tests

--- src/main.rs
+++ src/main.rs
@@ -22,35 +22,57 @@ fn main() {
     }
     println!("sledge: {}", cube.turns(&[R3, F1, R1, F3]));
 
-    for (lhs, rhs) in [
-        (U1, U3), (U2, U2), (U3, U1),
-        (R1, R3), (R2, R2), (R3, R1),
-        (F1, F3), (F2, F2), (F3, F1),
-        (D1, D3), (D2, D2), (D3, D1),
-        (B1, B3), (B2, B2), (B3, B1),
-        (L1, L3), (L2, L2), (L3, L1),
-    ] {
-        assert_eq!(corners * lhs, !(corners * rhs));
-        assert_eq!(edges * lhs, !(edges * rhs));
-        assert_eq!(cube * lhs, !(cube * rhs));
-    }
-
-    let s = corners.turns(&[R1, U1, R3, U3]);
-    assert_eq!(s * s, corners.turns(&[R1, U1, R3, U3, R1, U1, R3, U3]));
-
     for t in [U1, U2, U3, R1, R2, R3, F1, F2, F3] {
         println!("{:?} corner parity={}", t, (corners * t).parity());
     }
-    for (lhs, rhs) in [
-        (U1, true), (U2, false), (U3, true),
-        (R1, true), (R2, false), (R3, true),
-        (F1, true), (F2, false), (F3, true),
-        (D1, true), (D2, false), (D3, true),
-        (B1, true), (B2, false), (B3, true),
-        (L1, true), (L2, false), (L3, true),
-    ] {
-        assert_eq!((corners * lhs).parity(), rhs);
-        assert_eq!((edges * lhs).parity(), rhs);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use Turn::*;
+
+    #[test]
+    fn test_inverses() {
+        let corners = Corners::default();
+        let edges = Edges::default();
+        let cube = Cube::default();
+        for (lhs, rhs) in [
+            (U1, U3), (U2, U2), (U3, U1),
+            (R1, R3), (R2, R2), (R3, R1),
+            (F1, F3), (F2, F2), (F3, F1),
+            (D1, D3), (D2, D2), (D3, D1),
+            (B1, B3), (B2, B2), (B3, B1),
+            (L1, L3), (L2, L2), (L3, L1),
+        ] {
+            assert_eq!(corners * lhs, !(corners * rhs));
+            assert_eq!(edges * lhs, !(edges * rhs));
+            assert_eq!(cube * lhs, !(cube * rhs));
+        }
     }
-    assert!(!(corners.turns(&[R3, F1, R1, F3])).parity());
+
+    #[test]
+    fn test_parity() {
+        let corners = Corners::default();
+        let edges = Edges::default();
+        for (lhs, rhs) in [
+            (U1, true), (U2, false), (U3, true),
+            (R1, true), (R2, false), (R3, true),
+            (F1, true), (F2, false), (F3, true),
+            (D1, true), (D2, false), (D3, true),
+            (B1, true), (B2, false), (B3, true),
+            (L1, true), (L2, false), (L3, true),
+            ] {
+                assert_eq!((corners * lhs).parity(), rhs);
+                assert_eq!((edges * lhs).parity(), rhs);
+        }
+        assert!(!(corners.turns(&[R3, F1, R1, F3])).parity());
+    }
+
+    #[test]
+    fn test_mul() {
+        let corners = Corners::default();
+        let s = corners.turns(&[R1, U1, R3, U3]);
+        assert_eq!(s * s, corners.turns(&[R1, U1, R3, U3, R1, U1, R3, U3]));
+    }
 }
