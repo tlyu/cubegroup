b816663e65028f0061255c76000711f37378c0b0
Taylor Yu <code@argon.blue>
2026-05-16 18:31:56 -0500

    showmove example

---
+++ examples/showmove.rs
@@ -0,0 +1,28 @@
+use std::env;
+use std::error::Error;
+use std::io;
+
+use cubegroup::*;
+
+fn one(s: &str) -> Result<(), Box<dyn Error>> {
+    let t = s.parse::<Turns>().map_err(|_| "parse failure".to_string())?;
+    let c = Cube::default() * &t;
+    println!("{} position: {}", &t, c.speffz());
+    println!("{} cycles:   {}", &t, c.cycles().speffz());
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = env::args();
+    if args.len() > 1 {
+        for s in args.skip(1) {
+            one(&s)?;
+        }
+    } else {
+        eprintln!("Reading moves from standard input...");
+        for s in io::stdin().lines() {
+            one(&s?)?;
+        }
+    }
+    Ok(())
+}
