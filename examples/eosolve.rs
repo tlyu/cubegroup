24ef535c8090fd5b29fa033c1f3f9be493181b30
Taylor Yu <code@argon.blue>
2026-05-26 20:50:00 -0500

    EO solve example

---
+++ examples/eosolve.rs
@@ -0,0 +1,49 @@
+use std::ops::ControlFlow::{self, Break, Continue};
+
+use cubegroup::*;
+
+fn init_table() -> Vec<Vec<u16>> {
+    let mut v = Vec::new();
+    for i in 0u16..2048 {
+        let row: Vec<_> = allturns().into_iter().map(|t| {
+            (Edges::set_eo(i) * t).eo()
+        }).collect();
+        v.push(row);
+    }
+    v
+}
+
+fn idfs(init_eo: u16, depth: u8, prev_turn: Option<Turn>, table: &Vec<Vec<u16>>, v: &mut Vec<Turn>) -> ControlFlow<(), ()> {
+    if depth == 0 {
+        if init_eo == 0 {
+            if let Some(t) = prev_turn {
+                v.insert(0, t);
+            }
+            return Break(());
+        } else {
+            return Continue(());
+        }
+    }
+    canonturns(prev_turn).into_iter().try_for_each(|t| {
+        let eo = table[init_eo as usize][t as usize];
+        idfs(eo, depth - 1, Some(t), table, v)
+    }).map_break(|_| {
+        if let Some(t) = prev_turn {
+            v.insert(0, t)
+        }
+    })
+}
+
+fn main() {
+    let table = init_table();
+    for eo in 0..2048 {
+        let mut v = Vec::<Turn>::new();
+        let r = (0..8).try_for_each(|d| {
+            idfs(eo, d, None, &table, &mut v)
+        });
+        match r {
+            Break(_) => { println!("{eo:011b} {}", Turns::from(&v[..]))},
+            _ => { println!("{eo:011b} not found") },
+        }
+    }
+}
